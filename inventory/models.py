aab4ac359c88ad40e365d1161515e59e3bd5787e
ChristopherRoy1 <christopher.roy@mail.mcgill.ca>
2022-01-15 18:25:58 -0500

    added models. began implementation of item model.

---
+++ inventory/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+    The following class ... TODO
+'''
+class Item(models.Model):
+    sku = models.CharField(max_length=16)
+    company = models.ForeignKey('Company', on_delete=models.PROTECT)
+
+    weight_value = models.FloatField()
+    weight_unit = models.CharField(
+        max_length=2,
+        choices=WeightUnit.choices,
+        default=WeightUnit.KILO
+    )
+
+    # Fields to keep track of dimensions - important for shipment logic
+    dimension_x_value = models.FloatField()
+    dimension_y_value = models.FloatField()
+    dimension_z_value = models.FloatField()
+
+    dimension_unit = models.CharField(
+        max_length=2,
+    )
+
+
+    class DimensionUnit(models.TextChoices):
+        M = 'm', 'Meters'
+        FT = 'ft', 'Feet'
+
+    class WeightUnit(models.TextChoices):
+        KG = 'kg', 'Kilogram'
+        LB = 'lb', 'Pound'
+        OZ = 'oz', 'Ounces'
+
+
+
+'''
+    The following class ... TODO
+'''
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    legal_name = models.CharField(max_length=200)
+    pass
+
+
+'''
+    The following class ... TODO
+'''
+class ItemInventory(models.Model):
+    pass
+
+'''
+    The following class ... TODO
+'''
+class Shipment(models.Model):
+    pass
+
+'''
+    The following class ... TODO
+'''
+class ShipmentItem(models.Model):
+    pass
+
+
+'''
+    The following class ... TODO
+'''
+class SKUFormat(models.Model):
+    pass
